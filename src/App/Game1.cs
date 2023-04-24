1ad1a61a9e0150cfc53d777f872ab1251713222b
Tefatika <692720+Tefatika@users.noreply.github.com>
2023-04-24 11:39:25 +0200

    Added MonoGame project template

---
+++ src/App/Game1.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace App;
+
+public class Game1 : Game
+{
+    private readonly GraphicsDeviceManager graphics;
+    private SpriteBatch spriteBatch;
+    private SpriteFont helloWorldFont;
+
+    public Game1()
+    {
+        graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+    }
+
+    protected override void LoadContent()
+    {
+        spriteBatch = new SpriteBatch(GraphicsDevice);
+
+        helloWorldFont = Content.Load<SpriteFont>("Arial");
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+
+        spriteBatch.Begin();
+
+        spriteBatch.DrawString(helloWorldFont, "Hello World!", new(0f, 0f), Color.White);
+
+        spriteBatch.End();
+
+        base.Draw(gameTime);
+    }
+}
---
+++ src/App/Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new App.Game1();
+game.Run();
